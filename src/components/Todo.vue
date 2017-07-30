b212cd56b44d7b42fe37266a7f2f6d1a502c6364
irvinwatr93 <irvin@watr.mx>
2017-07-30 13:16:48 -0500

    mostrar formulario de edición en componente Todo al momento de clickear botón

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,26 +1,43 @@
 <template>
 <div>
     <div class='ui centered card'>
-      <div class='content'>
+        <div class="content" v-show="!isEditing">
         <div class='header'>
-          {{ todo.title }}
+            {{ todo.title }}
         </div>
         <div class='meta'>
-          {{ todo.project }}
+            {{ todo.project }}
         </div>
         <div class='extra content'>
-          <span class='right floated edit icon'>
+            <span class='right floated edit icon' v-on:click="showForm">
             <i class='edit icon'></i>
-          </span>
+            </span>
         </div>
-      </div>
-      <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-      </div>
-      <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-      </div>
-	</div>
+        </div>
+        <div class="content" v-show="isEditing">
+            <div class='ui form'>
+                <div class='field'>
+                <label>Title</label>
+                <input type='text' v-model="todo.title" >
+                </div>
+                <div class='field'>
+                <label>Project</label>
+                <input type='text' v-model="todo.project" >
+                </div>
+                <div class='ui two button attached buttons'>
+                <button class='ui basic blue button' v-on:click="hideForm">
+                    Close X
+                </button>
+                </div>
+            </div>
+        </div>
+        <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" >
+            Completed
+        </div>
+        <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
+            Pending
+        </div>
+    </div>
     <br>
 </div>
 </template>
@@ -28,5 +45,18 @@
 <script type="text/javascript">
     export default {
         props: ['todo'],
+        data() {
+            return {
+            isEditing: false,
+            };
+        },
+        methods: {
+            showForm() {
+            this.isEditing = true;
+            },
+            hideForm() {
+            this.isEditing = false;
+            },
+        },
     };
 </script>
